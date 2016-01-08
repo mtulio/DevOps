9d1c79e076be9a9b099c7041cce85c19aad0f097
Marco Tulio R Braga <git@mtulio.eng.br>
2016-01-08 01:51:03 -0200

    puppet::named - changing name of module: from named to dnssec

--- etc/puppet/modules/named/manifests/service.pp
+++
@@ -1,9 +0,0 @@
-class named::service ($service_name = $named::params::service_name) {
-  
-  service { 'named' :
-    name    => $service_name,
-    enable  => true,
-    ensure  => running,
-    require => Package['bind','bind-chroot'],
-  }
-}
