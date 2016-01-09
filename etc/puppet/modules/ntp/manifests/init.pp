1bf3491f32f33fe827ce5530fdd919528f9acfaa
Marco Tulio R Braga <git@mtulio.eng.br>
2016-01-09 17:16:37 -0200

    puppet::ntp - testing params initialization in class ntp - test2

--- etc/puppet/modules/ntp/manifests/init.pp
+++ etc/puppet/modules/ntp/manifests/init.pp
@@ -1,8 +1,11 @@
 
 # Test3: send args from class declaration on manifests/site.pp
+
 # Test2: send args from class declaration on tests/init.pp
+class ntp ($package) inherits ntp::params {
+
 # Test1: inherits from ntp::params 
-class ntp ($package = $ntp::params::package_name) inherits ntp::params {
+#class ntp ($package = $ntp::params::package_name) inherits ntp::params {
 
   # Install NTP 
   package { $package:
--- etc/puppet/modules/ntp/tests/init.pp
+++ etc/puppet/modules/ntp/tests/init.pp
@@ -1,1 +1,8 @@
-include ntp
+
+# Test1: inherits from ntp::params 
+#include ntp
+
+# Test2: send args from class declaration on tests/init.pp
+class { 'ntp' : package => 'ntp' , }
+
+# Test3: send args from class declaration on manifests/site.pp
