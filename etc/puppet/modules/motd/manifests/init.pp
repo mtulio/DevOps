bc9057431a6a13e3097daf209db14cc72c0bc105
Marco Tulio R Braga <git@mtulio.eng.br>
2016-01-10 02:53:24 -0200

    puppet::motd - update motd class, change template

--- etc/puppet/modules/motd/manifests/init.pp
+++ etc/puppet/modules/motd/manifests/init.pp
@@ -1,6 +1,12 @@
 class motd {
-    file { "/etc/motd":
-        ensure => 'file', # file exists resource. @check_doc
-        source => "puppet:///modules/motd/motd",    # path of file to be copied from master
+
+  file { "/etc/motd":
+    ensure => 'file', # file exists resource. @check_doc
+    #owner   => "root",
+    #group   => "root",
+    #mode    => "0644",
+    #source => "puppet:///modules/motd/motd",    # path of file to be copied from master
+    content => template('base/motd.erb'),
     }
+
 }
