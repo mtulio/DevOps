a4bd871854ff07243db2eb6343560cee8e4afb39
Marco Tulio R Braga <git@mtulio.eng.br>
2016-01-11 09:30:59 -0200

    Fix dependencies of last commit 'Add 'business layer'[roles] and 'implementation layer'[profiles] classes'

--- etc/puppet/modules/tests/manifests/drivemap.pp
+++
@@ -1,9 +0,0 @@
-define tests::drivemap($parameters = '-w 20% -c 10% -p') {
-    nrpebasic::command { "checkdisk ${name}":
-       ensure     => 'present',
-       command    => 'check_disk',
-       parameters => "${parameters} ${name}",
-    }
-
-  notice (" #> drivemap call")
-}
