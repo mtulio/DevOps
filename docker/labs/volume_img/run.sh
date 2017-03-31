273dcc251adc8d07cb1a749503aa4c3ead583ccd
Marco Tulio R Braga <marco.tulio@mtulio.eng.br>
2017-03-31 02:48:17 -0300

    [migration-docker-infra] removing docker labs files

--- docker/labs/volume_img/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Running two dockers
-
-docker run -i -t -d -v ${PWD}/www_root:/var/www/html teste:html /bin/bash
-docker run -i -t -d -v ${PWD}/www_root:/var/www/html teste:html /bin/bash
-
